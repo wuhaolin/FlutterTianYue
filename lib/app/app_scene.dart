6b9663577e9d1f6c658661b9fc864a9ef765aab2
wuhaolin <wuhaolin520@qq.com>
2020-07-13 15:32:28 +0800

    添加新项目

--- lib/app/app_scene.dart
+++ lib/app/app_scene.dart
@@ -6,13 +6,11 @@ import 'package:tianyue/public.dart';
 class AppScene extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    // 物料app是一个大的控件
     return MaterialApp(
       debugShowCheckedModeBanner: true,
       theme: ThemeData(
-        primaryColor: Colors.white,
-        dividerColor: Color(0xffeeeeee),
         scaffoldBackgroundColor: TYColor.paper,
-        textTheme: TextTheme(body1: TextStyle(color: TYColor.darkGray)),
       ),
       home: GuideScene(),
     );
