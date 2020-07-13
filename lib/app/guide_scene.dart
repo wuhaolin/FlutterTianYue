2ff3ad3979195db15dd25081585aa7a3b75024f1
wuhaolin <wuhaolin520@qq.com>
2020-07-13 16:57:46 +0800

    添加新项目

--- lib/app/guide_scene.dart
+++ lib/app/guide_scene.dart
@@ -11,41 +11,39 @@ class GuideScene extends StatefulWidget {
 }
 
 class GuideSceneState extends State<GuideScene> {
-  List<String> _guideList = [
+
+  List<String> imageList = [
     'img/guide1.png',
     'img/guide2.png',
     'img/guide3.png',
     'img/guide4.png',
   ];
-  List<String> _guideInfoList = [
+
+  List<String> textList = [
     "动漫陪你每一个夜晚",
     "同你去往每个地方",
     "懂你，更懂你所爱",
     "因为在意，所以用心",
   ];
 
-  List<Widget> _bannerList = [];
+  List<Widget> bannerList = [];
 
   @override
   void initState() {
     super.initState();
-    _initBannerData();
-  }
-
-  void _initBannerData() {
-    for (int i = 0, length = _guideList.length; i < length; i++) {
+    for (int i = 0, length = imageList.length; i < length; i++) {
       if (i == length - 1) {
-        _bannerList.add(Stack(
+        bannerList.add(Stack(
           children: <Widget>[
             Image.asset(
-              _guideList[i],
+              imageList[i],
               fit: BoxFit.cover,
               width: double.infinity,
               height: double.infinity,
             ),
             Center(
               child: new Text(
-                _guideInfoList[i],
+                textList[i],
                 style: new TextStyle(fontSize: 20, color: Colors.white),
               ),
             ),
@@ -67,10 +65,10 @@ class GuideSceneState extends State<GuideScene> {
                       style: TextStyle(color: Colors.white, fontSize: 14),
                     ),
                     decoration: new BoxDecoration(
-                        borderRadius: BorderRadius.all(Radius.circular(4.0)),
-                        color: Color(0X19FFFFFF),
-                        border:
-                            new Border.all(width: 1, color: Colors.white70)),
+                      borderRadius: BorderRadius.all(Radius.circular(4.0)),
+                      color: Color(0X19FFFFFF),
+                      border:
+                      new Border.all(width: 1, color: Colors.white70)),
                   ),
                 ),
               ),
@@ -78,17 +76,17 @@ class GuideSceneState extends State<GuideScene> {
           ],
         ));
       } else {
-        _bannerList.add(new Stack(
+        bannerList.add(new Stack(
           children: <Widget>[
             new Image.asset(
-              _guideList[i],
+              imageList[i],
               fit: BoxFit.cover,
               width: double.infinity,
               height: double.infinity,
             ),
             new Center(
               child: new Text(
-                _guideInfoList[i],
+                textList[i],
                 style: new TextStyle(fontSize: 20, color: Colors.white),
               ),
             )
@@ -110,7 +108,7 @@ class GuideSceneState extends State<GuideScene> {
                 padding: EdgeInsets.only(bottom: 32.0),
                 itemColor: Colors.white,
                 itemActiveColor: TYColor.primary),
-            children: _bannerList));
+            children: bannerList));
   }
 
   void _goMain() {
